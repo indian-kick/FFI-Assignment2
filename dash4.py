5fe35071714d6a2e9ec45e3931c6924946bb935f
indian-kick <kickbuttowskyy@gmail.com>
2025-06-18 03:46:52 +0100

    Update dash4.py

--- dash4.py
+++ dash4.py
@@ -39,7 +39,9 @@ df_softs = [load_data(os.path.join(folder_path, sf)) for sf in selected_soft_ind
 # # === Global Filter Controls ===
 # st.sidebar.markdown("### \U0001F5C2 Filter Timeframe")
 
-df_target['Reference Period'] = pd.to_datetime(df_target['Reference Period'])
+for df in df_softs:
+    df['Reference Period'] = pd.to_datetime(df['Reference Period'], errors='coerce')
+    df.dropna(subset=['Reference Period'], inplace=True)
 
 # min_date, max_date = df_target['Reference Period'].min().date(), df_target['Reference Period'].max().date()
 
